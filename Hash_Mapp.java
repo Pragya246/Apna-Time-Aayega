5116198514a1fd3220555c3940fd2e9a4b85caf5
Pragya246 <lspragyags@gmail.com>
2021-01-10 20:31:23 +0530

    my work

---
+++ Hash_Mapp.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.HashMap;
+public class Hash_Mapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("aalu ","lelo");
+		map.put("kanda ","lelo");
+		map.put("bheek ","lelo");
+		
+	}
+
+}
---
+++ Tree_Set.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Tree_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Set<String> s=new TreeSet<String>();
+		s.add("1");
+		s.add("2");
+		s.add("3");
+		s.forEach(d->System.out.print(d));
+		System.out.println(s.add("0"));
+		System.out.println(s.add("A"));
+		System.out.println(s.add("z"));
+		System.out.println(s.add("a"));
+		System.out.println(s.add("One"));
+		System.out.println(s.add("ONe"));
+		System.out.println(s.add("0wo"));
+		s.forEach(a->System.out.println(a));*/
+		 
+	}
+
+
+}
